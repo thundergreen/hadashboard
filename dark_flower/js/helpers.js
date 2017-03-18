9874f2eb05f66fac9f8aa59cac2aed91d562aea3
ThUnD3r|Gr33n <thundergreen@users.noreply.github.com>
2017-03-18 22:38:35 +0100

    Upladed Skins
    Here some Skins to play around :)

---
+++ dark_flower/js/helpers.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+  $("h1.title2").each(function()
+  {
+    if ($(this).is(":empty"))
+    {
+      $(this).closest("div").children("h1.title").css("line-height","35px");
+    }
+  });
+});
